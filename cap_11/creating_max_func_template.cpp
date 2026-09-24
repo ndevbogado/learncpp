ed6c944badea2d2cae73bf31267f3656f814a2c5
ndevbogado <ndevbogado@gmail.com>
2026-09-24 12:11:31 -0300

    Advance to section 11.8
    Create examples of previous section (function templates and generic
    programming) and advance to the next one

--- cap_11/creating_max_func_template.cpp
+++ cap_11/creating_max_func_template.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 
+template  <typename T>	//This is the template parameter declaration defining T as a type template		       //parameter
+T max(T x, T y){    // This is the function template definition for max<T>
+    return (x < y) ? y : x;
+}
+
 int main(){
+    std::cout << "Type two numbers to kwon which one is the greates of them: " << std::endl;
+    int x{};
+    std::cin >> x;
+    int y{};
+    std::cin >> y;
+
+    std::cout << max<int>(x, y) << " is the greates of the set." << std::endl;
     return 0;
 }
 
